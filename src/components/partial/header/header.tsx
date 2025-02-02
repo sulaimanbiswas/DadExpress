e7a4cb5b42650f9078dc1c513fa2069338dcf7d0
Md Suliman <Sulaimanbiswasbd@gmail.com>
2025-02-03 05:12:35 +0600

    sidebar submenu implement

--- src/components/partial/header/header.tsx
+++ src/components/partial/header/header.tsx
@@ -14,19 +14,19 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div
-      className="h-[50px] pr-6 w-full grid items-center grid-cols-[173px_0%_1fr] lg:grid-cols-[240px_50%_1fr]"
+      className="grid h-[50px] w-full grid-cols-[173px_0%_1fr] items-center pr-6 lg:grid-cols-[240px_50%_1fr]"
       style={{
         background:
           "linear-gradient(218.47deg, rgb(59, 58, 90) -74.13%, rgb(129, 78, 240) 106.5%)",
       }}
     >
       <div className="flex items-center">
-        <div className=" w-16 flex justify-center items-center flex-shrink-0">
-          <button className="flex justify-center items-center w-6 h-6">
+        <div className="flex w-16 flex-shrink-0 items-center justify-center">
+          <button className="flex h-6 w-6 items-center justify-center">
             <AlignJustify size={22} className="text-white" />
           </button>
         </div>
-        <Link href="/accounts" className="h-6 flex items-center mt-1.5">
+        <Link href="/accounts" className="mt-1.5 flex h-6 items-center">
           <Image
             className="dark:invert"
             src="/logo.svg"
@@ -38,24 +38,24 @@ const Header = () => {
         </Link>
       </div>
       <div className=""></div>
-      <div className="flex items-center w-full justify-end gap-3">
+      <div className="flex w-full items-center justify-end gap-3">
         <Link
           href="/notificaton"
-          className="h-8 w-10 bg-white/10 flex  justify-center items-center rounded-md "
+          className="flex h-8 w-10 items-center justify-center rounded-md bg-white/10"
         >
           <Bell className="text-white" size={20} fill="#fff" />
         </Link>
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <div className="h-[26px] w-[26px] bg-white flex justify-center items-center rounded-full p-1 cursor-pointer">
+            <div className="flex h-[26px] w-[26px] cursor-pointer items-center justify-center rounded-full bg-white p-1">
               <User strokeWidth={2.5} className="text-primary" />
             </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="w-[200px] pt-4">
             <DropdownMenuLabel>
-              <div className="flex w-full justify-center flex-col items-center gap-2">
-                <div className="h-10 w-10 bg-gray-400 flex justify-center items-center rounded-full p-1">
+              <div className="flex w-full flex-col items-center justify-center gap-2">
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 p-1">
                   <User strokeWidth={2.5} className="text-primary-foreground" />
                 </div>
                 <p className="text-xs font-normal">
@@ -66,15 +66,12 @@ const Header = () => {
             <DropdownMenuSeparator />
             <DropdownMenuItem className="text-xs">Settings</DropdownMenuItem>
             <DropdownMenuItem className="text-xs">
-              Notification
-            </DropdownMenuItem>
-            <DropdownMenuItem className="text-xs">
               Trams of use
             </DropdownMenuItem>
             <DropdownMenuItem className="text-xs">
               Change Password
             </DropdownMenuItem>
-            <DropdownMenuItem className="flex justify-center items-center w-full bg-gray-100 font-medium text-xs mt-2">
+            <DropdownMenuItem className="mt-2 flex w-full items-center justify-center bg-gray-100 text-xs font-medium">
               Log out
             </DropdownMenuItem>
             <DropdownMenuArrow className="fill-white" />
--- src/components/partial/sidebar/sidebar.tsx
+++ src/components/partial/sidebar/sidebar.tsx
@@ -1,13 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoWalletOutline } from "react-icons/io5";
+import { SiMeta } from "react-icons/si";
 
 const Sidebar = () => {
+  const isActive = false;
+  const isSubMenuExpand = true;
   return (
-    <aside className="w-[240px] bg-white flex-shrink-0 border-r border-[#e4e6eb] pt-1.5 overflow-hidden ">
-      <div className="flex gap-3 pt-3.5 pr-5 p-[18px] border-b border-[#e4e6eb] flex-col">
-        <div className="flex gap-2 w-full cursor-pointer py-2 px-2 font-medium flex-col justify-between bg-[#f5f6f7] rounded-md">
+    <aside className="w-[240px] flex-shrink-0 overflow-hidden border-r border-[#e4e6eb] bg-white pt-1.5">
+      <div className="flex flex-col gap-3 border-b border-[#e4e6eb] p-[18px] pr-5 pt-3.5">
+        <div className="flex w-full cursor-pointer flex-col justify-between gap-2 rounded-md bg-[#f5f6f7] px-2 py-2 font-medium">
           <Image
-            className="dark:invert w-[22px] h-[22px]"
+            className="h-[22px] w-[22px] dark:invert"
             src="/usa.svg"
             alt="Next.js logo"
             width={40}
@@ -19,10 +28,163 @@ const Sidebar = () => {
             <div>$ 2,28</div>
           </div>
         </div>
-        <Button className="h-8 bg-pink-200/50 hover:bg-pink-200/60 text-pink-500 text-xs font-medium">
+        <Button className="h-8 bg-pink-200/50 text-xs font-medium text-pink-500 hover:bg-pink-200/60">
           Top up the balance
         </Button>
       </div>
+      <ScrollArea className="h-[calc(100vh-205px)] overflow-y-auto">
+        <div
+          className={cn(
+            "transition-all hover:bg-gray-100",
+            isActive && "bg-gray-100 transition-all",
+          )}
+        >
+          <Link href="/" className="">
+            <div className="flex h-12 items-center">
+              <div className="flex w-16 items-center justify-center">
+                <LayoutDashboard
+                  className={cn("text-gray-400", isActive && "text-primary")}
+                  size={20}
+                />
+              </div>
+              <span
+                className={cn(
+                  "text-sm font-medium text-gray-950",
+                  isActive && "text-primary",
+                )}
+              >
+                Dashboard
+              </span>
+            </div>
+          </Link>
+        </div>
+        <div
+          className={cn(
+            "transition-all hover:bg-gray-100",
+            isActive && "bg-gray-100 transition-all",
+          )}
+        >
+          <Link href="/" className="">
+            <div className="flex h-12 items-center">
+              <div className="flex w-16 items-center justify-center">
+                <SiMeta
+                  className={cn("text-gray-400", isActive && "text-primary")}
+                  size={20}
+                />
+              </div>
+              <span
+                className={cn(
+                  "text-sm font-medium text-gray-950",
+                  isActive && "text-primary",
+                )}
+              >
+                Ad Accounts
+              </span>
+            </div>
+          </Link>
+        </div>
+        <div
+          className={cn(
+            "transition-all hover:bg-gray-100",
+            isActive && "bg-gray-100 transition-all",
+          )}
+        >
+          <div className="relative flex h-12 items-center justify-between">
+            <div className="flex items-center">
+              <div className={cn("flex w-16 items-center justify-center")}>
+                <div className="absolute top-6 h-8 w-9 bg-gray-100"></div>
+                <div
+                  className={cn(
+                    isSubMenuExpand &&
+                      "z-50 flex h-9 w-9 items-center justify-center rounded-full bg-gray-400",
+                  )}
+                >
+                  <IoWalletOutline
+                    className={cn(
+                      "text-gray-400",
+                      isSubMenuExpand && "text-primary-foreground",
+                    )}
+                    size={20}
+                  />
+                </div>
+              </div>
+              <span
+                className={cn(
+                  "text-sm font-medium text-gray-950",
+                  isActive && "text-primary",
+                )}
+              >
+                Finance
+              </span>
+            </div>
+            <div className="flex items-center pr-5">
+              <IoIosArrowUp className="text-gray-600" />
+            </div>
+          </div>
+        </div>
+        <div className="mb-4">
+          <div
+            className={cn(
+              "transition-all hover:bg-gray-100",
+              isActive && "bg-gray-100 transition-all",
+            )}
+          >
+            <Link href="/" className="">
+              <div className="flex h-10 items-center">
+                <div className="flex w-16 items-center justify-center">
+                  <div className="z-10 flex h-10 w-9 items-center justify-center bg-gray-100">
+                    <LayoutDashboard
+                      className={cn(
+                        "text-gray-400",
+                        isActive && "text-primary",
+                      )}
+                      size={20}
+                    />
+                  </div>
+                </div>
+                <span
+                  className={cn(
+                    "text-sm font-medium text-gray-950",
+                    isActive && "text-primary",
+                  )}
+                >
+                  Dashboard
+                </span>
+              </div>
+            </Link>
+          </div>
+          <div
+            className={cn(
+              "transition-all hover:bg-gray-100",
+              isActive && "bg-gray-100 transition-all",
+            )}
+          >
+            <Link href="/" className="">
+              <div className="flex h-10 items-center">
+                <div className="flex w-16 items-center justify-center">
+                  <div className="z-10 flex h-14 w-9 items-center justify-center rounded-b-2xl bg-gray-100">
+                    <LayoutDashboard
+                      className={cn(
+                        "text-gray-400",
+                        isActive && "text-primary",
+                      )}
+                      size={20}
+                    />
+                  </div>
+                </div>
+                <span
+                  className={cn(
+                    "text-sm font-medium text-gray-950",
+                    isActive && "text-primary",
+                  )}
+                >
+                  Dashboard
+                </span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </ScrollArea>
     </aside>
   );
 };
